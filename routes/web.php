e223bab78293a7dca43a91d28d07a2fb8ef66cbf
EC2 Default User <ec2-user@ip-172-31-38-14.ap-northeast-1.compute.internal>
2022-12-26 10:54:52 +0000

    PHP-Laravel-12

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 // Route::controller(AAAController::class)->group(function(){
@@ -28,8 +28,8 @@ Route::controller(NewsController::class)->prefix('admin')->group(function() {
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->group(function(){
-    Route::get('admin/profile/create','add');
-    Route::get('admin/profile/edit','edit');
+    Route::get('admin/profile/create','add')->middleware('auth');
+    Route::get('admin/profile/edit','edit')->middleware('auth');;
 });
 Auth::routes();
 
