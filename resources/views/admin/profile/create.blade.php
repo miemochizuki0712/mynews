8b9de6031c8c411576474f5f3b569906915d972a
EC2 Default User <ec2-user@ip-172-31-38-14.ap-northeast-1.compute.internal>
2022-12-30 12:28:02 +0000

    PHP-Laravel-13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,7 +11,52 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>My プロフィール</h2>
-            </div>
+                
+    <form action="{{ route('admin.profile.create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                
+        <div class="form-group row">
+            <label class="col-md-2">氏名</label>
+              <div class="col-md-10">
+                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+              </div>
         </div>
+        
+        <div class="form-group row">
+            <label class="col-md-2">性別</label>
+               <div class="col-md-10">
+                 <select class="form-control" name="gender" value="{{ old('gender') }}">
+                  <option value="未選択">選択してください</option>
+                  <option value="男性">男性</option>
+                  <option value="女性">女性</option>
+                 </select>
+        　　　</div>
+        </div>
+        
+        <div class="form-group row">
+            <label class="col-md-2">趣味</label>
+               <div class="col-md-10">
+                 <textarea class="form-control" name ="hobby" rows="5">{{ old('hobby') }}</textarea>
+                </div>
+        </div>
+        
+        <div class="form-group row">
+            <label class="col-md-2">自己紹介欄</label>
+               <div class="col-md-10">
+                 <textarea class="form-control" name ="introduction" rows="10">{{ old('introduction') }}</textarea>
+                </div>
+        </div>
+        
+         <input type="submit" class="btn btn-primary" value="更新">
+        
+{{--氏名(name)、性別(gender)、趣味(hobby)、自己紹介欄(introduction)--}}
+        
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,21 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
+
 
 // Route::controller(AAAController::class)->group(function(){
 //     Route::get('XXX','bbb');
 // });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->group(function(){
-    Route::get('admin/profile/create','add')->middleware('auth');
-    Route::get('admin/profile/edit','edit')->middleware('auth');;
+Route::controller(ProfileController::class)->name('admin.')->middleware('auth')->group(function(){
+    Route::get('admin/profile/create','add')->name('news.add');
+    Route::post('admin/profile/create', 'create')->name('profile.create');
+    Route::get('admin/profile/edit','edit')->middleware('auth');
 });
 Auth::routes();
 
