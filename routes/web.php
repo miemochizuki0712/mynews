aa2e053720b116e4298863212b408eed9a402d64
EC2 Default User <ec2-user@ip-172-31-38-14.ap-northeast-1.compute.internal>
2023-01-03 10:28:02 +0000

    PHP-Laravel-13

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,10 @@ Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middl
 // });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->name('admin.')->middleware('auth')->group(function(){
-    Route::get('admin/profile/create','add')->name('news.add');
-    Route::post('admin/profile/create', 'create')->name('profile.create');
-    Route::get('admin/profile/edit','edit')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::get('profile/create','add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('profile/edit','edit')->name('profile.edit');
 });
 Auth::routes();
 
