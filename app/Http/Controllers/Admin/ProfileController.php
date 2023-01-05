47a6f85e83e4783bc3e526653422969a653dda02
EC2 Default User <ec2-user@ip-172-31-38-14.ap-northeast-1.compute.internal>
2023-01-05 10:03:01 +0000

    PHP-Laravel-14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,27 +4,37 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Profile;
 
 class ProfileController extends Controller
 {
-    //
-    public function add()
-    {
-        return view('admin.profile.create');
-    }
+  //
+  public function add()
+  {
+    return view('admin.profile.create');
+  }
 
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
+  public function create(Request $request)
+  {
+    $this->validate($request, Profile::$rules);
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
+    $profile = new Profile;
+    $form = $request->all();
 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+    // データベースに保存する
+    $profile->fill($form);
+    $profile->save();
+
+    return redirect('admin/profile/create');
+  }
+
+  public function edit()
+  {
+    return view('admin.profile.edit');
+  }
+
+  public function update()
+  {
+     return redirect('admin/profile/edit');
+  }
 }
